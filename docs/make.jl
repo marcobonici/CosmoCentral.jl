39b385ef38520e245ef3b15d09794e2a0bdb7c8a
Marco Bonici <marco.bonici@ge.infn.it>
2020-12-30 00:53:22 +0100

    Lil fix

--- docs/make.jl
+++ docs/make.jl
@@ -4,15 +4,5 @@ using CosmoCentral
 push!(LOAD_PATH,"../src/")
 makedocs(
     sitename = "CosmoCentral.jl Documentation",
-    pages = [
-       "Index" => "index.md",
-       "anotherPage" => "anotherPage.md"
-    ],
-    format = Documenter.HTML(),
-    modules = [CosmoCentral]
-)
-
-deploydocs(
-    repo = "github.com/marcobonici/CosmoCentral.jl.git",
-    devbranch = "main"
+    authors="Marco Bonici"
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,9 @@
 # CosmoCentral.jl
 
-Documentation for CosmoCentral.jl
+Documentation for l.jl. This is a Julia package to perform cosmological calculations.
+
+# CosmoCentral.jl Documentation
+
+```@docs
+ComputeAdimensionalHubbleFactor
+```
