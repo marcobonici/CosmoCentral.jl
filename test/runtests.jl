8053cb7124b70532e1f5912dd1ef1a66bb19f4be
marcobonici <bonici.marco@gmail.com>
2020-12-29 18:15:29 +0100

    lil change in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using CosmoCentral
 using Test
 
-ciccio = CosmoCentral.ComputeAdimensionalHubbleFactor(0.)
+test_E_z = CosmoCentral.ComputeAdimensionalHubbleFactor(0.)
+@test test_E_z == 1.
 
-@test ciccio == 1.
+test_H_z = CosmoCentral.ComputeHubbleFactor(0.)
+@test test_H_z == 67.
 
-ciccio = CosmoCentral.ComputeHubbleFactor(0.)
-@test ciccio == 67.
-println("Everything work!")
+test_r_z = CosmoCentral.ComputeComovingDistance(0.)
+@test test_r_z == 0.
