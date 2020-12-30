b541db203c273d0a05e06679b04daa83b08da4ff
Marco Bonici <marco.bonici@ge.infn.it>
2020-12-30 11:52:39 +0100

    Fix in make docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,18 @@
 using Documenter, CosmoCentral
 
 push!(LOAD_PATH,"../src/")
+
 makedocs(
-    sitename = "CosmoCentral.jl Documentation",
-    authors="Marco Bonici",
+    modules = [CosmoCentral],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    sitename = "TaylorSeries.jl",
+    authors  = "Luis Benet and David P. Sanders",
+    pages = [
+        "Home" => "index.md",
+        "Background" => "anotherPage.md",
+    ]
+)
+
+deploydocs(
     repo = "github.com/marcobonici/CosmoCentral.jl.git",
 )
