3afcfe88d8d9d9479a6242f4395f54ccf09f1ea1
marcobonici <bonici.marco@gmail.com>
2020-12-30 20:12:11 +0100

    lil change

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,19 @@
 using CosmoCentral
 using Test
 
-cosmo = CosmoCentral.w0waCDMParameters()
+params = CosmoCentral.w0waCDMParameters()
 
-test_E_z = CosmoCentral.ComputeAdimensionalHubbleFactor(0., cosmo)
-@test test_E_z == 1.
+@testset "Adimensional Hubble parameter at redshift zero is equal to one" begin
+    test_E_z = CosmoCentral.ComputeAdimensionalHubbleFactor(0., params)
+    @test test_E_z == 1.
+end
 
-test_H_z = CosmoCentral.ComputeHubbleFactor(0., cosmo1)
-@test test_H_z == 67.
+@testset "Adimensional Hubble parameter at redshift zero is equal to default value" begin
+    test_H_z = CosmoCentral.ComputeHubbleFactor(0., params)
+    @test test_H_z == params.H0
+end
 
-test_r_z = CosmoCentral.ComputeComovingDistance(0., cosmo)
-@test test_r_z == 0.
+@testset "Comoving distance at redshift zero is equal to zero" begin
+    test_r_z = CosmoCentral.ComputeComovingDistance(0., params)
+    @test test_r_z == 0.
+end
