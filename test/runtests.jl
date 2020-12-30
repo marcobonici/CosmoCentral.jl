9eb8cb47d70ef2b3cd0afe1abb9c5e28f26f5e33
marcobonici <bonici.marco@gmail.com>
2020-12-30 19:59:45 +0100

    Lil fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ cosmo = CosmoCentral.w0waCDMParameters()
 test_E_z = CosmoCentral.ComputeAdimensionalHubbleFactor(0., cosmo)
 @test test_E_z == 1.
 
-test_H_z = CosmoCentral.ComputeHubbleFactor(0., cosmo)
+test_H_z = CosmoCentral.ComputeHubbleFactor(0., cosmo1)
 @test test_H_z == 67.
 
 test_r_z = CosmoCentral.ComputeComovingDistance(0., cosmo)
