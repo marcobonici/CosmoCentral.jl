406c238ab7717b177063e8e5b00b02b4bd2a0ddf
Marco Bonici <marco.bonici@ge.infn.it>
2020-12-31 16:46:20 +0100

    Fixed tuple multiplication in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,6 +23,6 @@ end
     test_density = CosmoCentral.NormalizeAnalitycalDensityStruct(density)
     normalization, err = QuadGK.quadgk(x -> 1 /
     ComputeDensityFunction(x, densityparameters), densityparameters.zmin,
-    densityparameters.zmax, rtol=1e-12)*densityparameters.surfacedensity
-    @test normalization == test_density.normalization
+    densityparameters.zmax, rtol=1e-12)
+    @test normalization*densityparameters.surfacedensity == test_density.normalization
 end
