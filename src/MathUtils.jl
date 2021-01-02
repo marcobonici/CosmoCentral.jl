431f80d5e0870a2fabfd7b8764994d8d6f0d0050
marcobonici <bonici.marco@gmail.com>
2021-01-02 11:37:21 +0100

    Fixed typo in logspace

--- src/MathUtils.jl
+++ src/MathUtils.jl
@@ -2,6 +2,5 @@ function LogSpaced(minarray::Float64, maxarray::Float64, n::Int64)
     logmin = log10(minarray)
     logmax = log10(maxarray)
     logarray = Array(LinRange(logmin, logmax, n))
-    array = exp10.(array)
-    return array
+    return exp10.(logarray)
 end
