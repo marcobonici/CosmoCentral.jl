f841c0148fde710103a148a718b21b773844396a
Marco Bonici <marco.bonici@ge.infn.it>
2021-01-05 15:30:25 +0100

    Little fix in weight functions

--- src/WeightFunctions.jl
+++ src/WeightFunctions.jl
@@ -5,6 +5,7 @@ abstract type WeightFunction end
     PowerSpectrumGrid::PowerSpectrumGrid = PowerSpectrumGridStruct()
     w0waCDMCosmology::w0waCDMCosmology = w0waCDMStruct()
     Bias::Bias = PiecewiseBiasStruct()
+    BackgroundQuantities::BackgroundQuantities = BackgroundQuantitiesStruct()
     WeightFunctionArray::AbstractArray{Float64, 2} =
     ones(length(ConvolvedDensity.zbinarray)-1, length(PowerSpectrumGrid.zgrid))
 end
@@ -26,27 +27,6 @@ function ComputeGalaxyClusteringWeightFunction(z::Float64, i::Int64,
     ComputeHubbleFactor(z, w0waCDMCosmology) / c_0
 end
 
-"""
-    ComputeGalaxyClusteringWeightFunction(WeightFunction::WeightFunction)
-
-This function returns the source density for a given redshift ``z``.
-"""
-function ComputeGalaxyClusteringWeightFunctionOverGrid(
-    WeightFunction::WeightFunction)
-    for idx_zbinarray in 1:length(WeightFunction.ConvolvedDensity.zbinarray)-1
-        for idx_zgrid in 1:length(WeightFunction.PowerSpectrumGrid.zgrid)
-            WeightFunction.WeightFunctionArray[idx_zbinarray, idx_zgrid] =
-            ComputeGalaxyClusteringWeightFunction(
-            WeightFunction.PowerSpectrumGrid.zgrid[idx_zgrid],
-            idx_zbinarray,
-            WeightFunction.ConvolvedDensity,
-            WeightFunction.w0waCDMCosmology,
-            WeightFunction.Bias)
-        end
-    end
-end
-
-
 
 """
     ComputeGalaxyClusteringWeightFunction(PowerSpectrumGrid::PowerSpectrumGrid,
@@ -56,16 +36,15 @@ end
 This function returns the source density for a given redshift ``z``.
 """
 function ComputeGalaxyClusteringWeightFunctionOverGrid(
-    WeightFunction::WeightFunction,
-    BackgroundQuantities::BackgroundQuantities,
-    ConvolvedDensity::ConvolvedDensity)
+    WeightFunction::WeightFunction)
     c_0 = 2.99792458e5 #TODO: find a package containing the exact value of
                        #physical constants involved in calculations
     for idx_zbinarray in 1:length(WeightFunction.ConvolvedDensity.zbinarray)-1
         for idx_zgrid in 1:length(WeightFunction.PowerSpectrumGrid.zgrid)
             WeightFunction.WeightFunctionArray[idx_zbinarray, idx_zgrid] =
-            ConvolvedDensity.densitygridarray[idx_zbinarray, idx_zgrid] *
-            BackgroundQuantities.Hzgrid[idx_zgrid] / c_0
+            WeightFunction.Bias.BiasArray[idx_zbinarray, idx_zgrid]*
+            WeightFunction.ConvolvedDensity.densitygridarray[idx_zbinarray, idx_zgrid] *
+            WeightFunction.BackgroundQuantities.Hzgrid[idx_zgrid] / c_0
         end
     end
 end
