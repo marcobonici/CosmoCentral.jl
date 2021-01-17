6556722a388585713e6777b802030ead7ae9aee4
Marco Bonici <marco.bonici@ge.infn.it>
2021-01-17 19:58:30 +0100

    Improved IO

--- src/FileIO.jl
+++ src/FileIO.jl
@@ -30,9 +30,51 @@ function WritePowerSpectrumBackground(PowerSpectrum::PowerSpectrum,
     "power_spectrum/k_grid",
     CosmologicalGrid.KArray)
     h5write(Filename*".h5",
-    "power_spectrum/lin_p_mm_z_k",
+    "power_spectrum/lin_p_mm_k_z",
     PowerSpectrum.PowerSpectrumLinArray)
     h5write(Filename*".h5",
-    "power_spectrum/nonlin_p_mm_z_k",
+    "power_spectrum/nonlin_p_mm_k_z",
     PowerSpectrum.PowerSpectrumNonlinArray)
+end
+
+function ReadPowerSpectrumBackground(Filename::String,
+    MultipolesArray::Vector{Float64})
+    Filename *= ".h5"
+    file = HDF5.h5open(Filename, "r")
+    nonlin_p_mm_k_z = HDF5.read(file["power_spectrum"]["nonlin_p_mm_k_z"])
+    lin_p_mm_k_z    = HDF5.read(file["power_spectrum"]["lin_p_mm_k_z"])
+    z_grid = HDF5.read(file["power_spectrum"]["z_grid"])
+    k_grid = HDF5.read(file["power_spectrum"]["k_grid"])
+    comoving_distance_array = HDF5.read(file["cosmology"]["comoving_distance_array"])
+    hubble_array = HDF5.read(file["cosmology"]["hubble_array"])
+    BackgroundQuantities = BackgroundQuantitiesStruct(HZArray = hubble_array,
+    rZArray = comoving_distance_array)
+    CosmologicalGrid = CosmologicalGridStruct(ZArray = z_grid, KArray = k_grid,
+    MultipolesArray = MultipolesArray)
+    PowerSpectrum = PowerSpectrumStruct(PowerSpectrumLinArray = lin_p_mm_k_z,
+    PowerSpectrumNonlinArray = nonlin_p_mm_k_z,
+    InterpolatedPowerSpectrum = zeros(length(CosmologicalGrid.MultipolesArray), length(CosmologicalGrid.ZArray)))
+    return PowerSpectrum, BackgroundQuantities, CosmologicalGrid
+end
+
+function ReadPowerSpectrumBackgroundSeyfert(Filename::String,
+    MultipolesArray::Vector{Float64})
+    c_0 = 2.99792458e5
+    Filename *= ".h5"
+    file = HDF5.h5open(Filename, "r")
+    nonlin_p_mm_k_z = HDF5.read(file["power_spectrum"]["nonlin_p_mm_z_k"])
+    lin_p_mm_k_z    = HDF5.read(file["power_spectrum"]["lin_p_mm_z_k"])
+    z_grid = HDF5.read(file["power_spectrum"]["z_grid"])
+    k_grid = HDF5.read(file["power_spectrum"]["k_grid"])
+    dimensionless_comoving_distance_array = HDF5.read(file["cosmology"]["dimensionless_comoving_distance_array"])
+    dimensionless_hubble_array = HDF5.read(file["cosmology"]["dimensionless_hubble_array"])
+    BackgroundQuantities = BackgroundQuantitiesStruct(HZArray =
+    dimensionless_hubble_array*67,
+    rZArray = dimensionless_comoving_distance_array*c_0/67)
+    CosmologicalGrid = CosmologicalGridStruct(ZArray = z_grid, KArray = k_grid,
+    MultipolesArray = MultipolesArray)
+    PowerSpectrum = PowerSpectrumStruct(PowerSpectrumLinArray = lin_p_mm_k_z,
+    PowerSpectrumNonlinArray = nonlin_p_mm_k_z,
+    InterpolatedPowerSpectrum = zeros(length(CosmologicalGrid.MultipolesArray), length(CosmologicalGrid.ZArray)))
+    return PowerSpectrum, BackgroundQuantities, CosmologicalGrid
 end
