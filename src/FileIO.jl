26bfc6dca187d892ae753d48698640bd14172e72
Marco Bonici <marco.bonici@ge.infn.it>
2021-01-17 02:05:39 +0100

    Added IO power spectrum

--- src/FileIO.jl
+++ src/FileIO.jl
@@ -10,3 +10,29 @@ function WriteAngularCoefficients(AngularCoefficients::AngularCoefficients,
     "weight_functions/PhotometricGalaxy/density/norm_density_iz",
     ConvolvedDensity.DensityNormalizationArray)
 end
+
+function WritePowerSpectrumBackground(PowerSpectrum::PowerSpectrum,
+    BackgroundQuantities::BackgroundQuantities,
+    CosmologicalGrid::CosmologicalGrid, Filename::String)
+    h5write(Filename*".h5",
+    "cosmology/comoving_distance_array",
+    BackgroundQuantities.rZArray)
+    h5write(Filename*".h5",
+    "cosmology/hubble_array",
+    BackgroundQuantities.HZArray)
+    h5write(Filename*".h5",
+    "cosmology/z_grid",
+    CosmologicalGrid.ZArray)
+    h5write(Filename*".h5",
+    "power_spectrum/z_grid",
+    CosmologicalGrid.ZArray)
+    h5write(Filename*".h5",
+    "power_spectrum/k_grid",
+    CosmologicalGrid.KArray)
+    h5write(Filename*".h5",
+    "power_spectrum/lin_p_mm_z_k",
+    PowerSpectrum.PowerSpectrumLinArray)
+    h5write(Filename*".h5",
+    "power_spectrum/nonlin_p_mm_z_k",
+    PowerSpectrum.PowerSpectrumNonlinArray)
+end
