781a5ff55017963519019c5892357e8634bdfeb5
Marco Bonici <marco.bonici@ge.infn.it>
2021-04-17 21:34:06 +0200

    Another bug fixed in the beyond limber evaluation

--- src/SourceFunctions.jl
+++ src/SourceFunctions.jl
@@ -3,7 +3,8 @@ function ComputeSourceFunctionOverGrid(
     ConvolvedDensity::AsbtractConvolvedDensity,
     CosmologicalGrid::CosmologicalGrid,
     BackgroundQuantities::BackgroundQuantities,
-    w0waCDMCosmology::w0waCDMCosmologyStruct)
+    w0waCDMCosmology::w0waCDMCosmologyStruct,
+    PowerSpectrum::PowerSpectrumStruct)
     c_0 = 2.99792458e5 #TODO: find a package containing the exact value of
                        #physical constants involved in calculations
     for idx_ZBinArray in 1:length(ConvolvedDensity.ZBinArray)-1
@@ -11,7 +12,9 @@ function ComputeSourceFunctionOverGrid(
             LensingSourceFunction.SourceFunctionArray[idx_ZBinArray, idx_ZArray] =
             1.5 * (w0waCDMCosmology.H0/c_0)^2 * w0waCDMCosmology.ΩM *
             (1. + CosmologicalGrid.ZArray[idx_ZArray]) *
-            LensingSourceFunction.LensingEfficiencyArray[idx_ZBinArray, idx_ZArray]
+            LensingSourceFunction.LensingEfficiencyArray[idx_ZBinArray, idx_ZArray] *
+            PowerSpectrum.GrowthFactor[idx_ZArray] /
+            PowerSpectrum.GrowthFactor[1]
         end
     end
 end
