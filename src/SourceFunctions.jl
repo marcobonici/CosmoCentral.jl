1d25f8bde515630b591c328698fadff7d5be7c1d
Marco Bonici <marco.bonici@ge.infn.it>
2021-04-18 22:02:33 +0200

    Lil fix and cosmetics

--- src/SourceFunctions.jl
+++ src/SourceFunctions.jl
@@ -7,14 +7,12 @@ function ComputeSourceFunctionOverGrid(
     PowerSpectrum::PowerSpectrumStruct)
     c_0 = 2.99792458e5 #TODO: find a package containing the exact value of
                        #physical constants involved in calculations
-    for idx_ZBinArray in 1:length(ConvolvedDensity.ZBinArray)-1
-        for idx_ZArray in 1:length(CosmologicalGrid.ZArray)
-            LensingSourceFunction.SourceFunctionArray[idx_ZBinArray, idx_ZArray] =
+    for zbinidx in 1:length(ConvolvedDensity.ZBinArray)-1
+        for zidx in 1:length(CosmologicalGrid.ZArray)
+            LensingSourceFunction.SourceFunctionArray[zbinidx, zidx] =
             1.5 * (w0waCDMCosmology.H0/c_0)^2 * w0waCDMCosmology.ΩM *
-            (1. + CosmologicalGrid.ZArray[idx_ZArray]) *
-            LensingSourceFunction.LensingEfficiencyArray[idx_ZBinArray, idx_ZArray] *
-            PowerSpectrum.GrowthFactor[idx_ZArray] /
-            PowerSpectrum.GrowthFactor[1]
+            (1. + CosmologicalGrid.ZArray[zidx]) *
+            LensingSourceFunction.LensingEfficiencyArray[zbinidx, zidx]
         end
     end
 end
