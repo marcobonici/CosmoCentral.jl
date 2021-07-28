25b61339e87a5280917d2a5d8290b7e97dce99fc
Marco Bonici <marco.bonici@ge.infn.it>
2021-07-28 02:06:57 +0200

    First version of the MCMC step. precision 0.4%

--- src/MCMCUtils.jl
+++ src/MCMCUtils.jl
@@ -4,35 +4,37 @@ function EvaluateCℓMCMCStep(Cosmology::AbstractCosmology, Density::AbstractCon
     rZArray=zeros(length(CosmoGrid.ZArray)))
     ComputeBackgroundQuantitiesGrid!(CosmoGrid, backgroundquantities, Cosmology)
     ClassyParams = Initializeclassy(Cosmology)
-    PowerSpectrum = PowerSpectrum(PowerSpectrumLinArray =
+    Pmm = PowerSpectrum(PowerSpectrumLinArray =
     zeros(length(CosmoGrid.KArray), length(CosmoGrid.ZArray)),
     PowerSpectrumNonlinArray = zeros(length(CosmoGrid.KArray), length(CosmoGrid.ZArray)),
-    InterpolatedPowerSpectrum = zeros(length(CosmoGrid.ℓBinCenters), length(CosmoGrid.ZArray)))
-    EvaluatePowerSpectrum!(ClassyParams, CosmoGrid, PowerSpectrum)
+    InterpolatedPowerSpectrum = zeros(length(CosmoGrid.ℓBinCenters),
+    length(CosmoGrid.ZArray)))
+    EvaluatePowerSpectrum!(ClassyParams, CosmoGrid, Pmm)
     ComputeLimberArray!(CosmoGrid, backgroundquantities)
-    InterpolatePowerSpectrumLimberGrid!(CosmoGrid, backgroundquantities, PowerSpectrum,
+    InterpolatePowerSpectrumLimberGrid!(CosmoGrid, backgroundquantities, Pmm,
     BSplineCubic())
-    ExtractGrowthFactor!(backgroundquantities, PowerSpectrum)
-    GCWeightFunction = GCWeightFunction(WeightFunctionArray =
-    zeros(length(Density.DensityNormalizationArray), length(CosmoGrid.ZArray)), BiasKind = Bias)
-    CosmoCentral.ComputeBiasGrid!(CosmoGrid, GCWeightFunction, Density)
-    CosmoCentral.ComputeWeightFunctionGrid!(GCWeightFunction, Density, CosmoGrid,
+    ExtractGrowthFactor!(backgroundquantities, Pmm)
+    GCW = GCWeightFunction(WeightFunctionArray =
+    zeros(length(Density.DensityNormalizationArray), length(CosmoGrid.ZArray)),
+    BiasKind = Bias)
+    CosmoCentral.ComputeBiasGrid!(CosmoGrid, GCW, Density)
+    CosmoCentral.ComputeWeightFunctionGrid!(GCW, Density, CosmoGrid,
     backgroundquantities, Cosmology)
-    WLWeightFunction = WLWeightFunction(WeightFunctionArray =
+    WLW = WLWeightFunction(WeightFunctionArray =
     zeros(length(Density.DensityNormalizationArray), length(CosmoGrid.ZArray)),
     LensingEfficiencyArray = zeros(length(Density.DensityNormalizationArray),
-    length(CosmoGrid.ZArray)))
-    ComputeLensingEfficiencyGrid!(WLWeightFunction, Density, CosmoGrid,
+    length(CosmoGrid.ZArray)), IntrinsicAlignmentModel = IA)
+    ComputeLensingEfficiencyGrid!(WLW, Density, CosmoGrid,
     backgroundquantities, Cosmology, CustomLensingEfficiency())
-    ComputeIntrinsicAlignmentGrid!(CosmoGrid, WLWeightFunction, Density,
+    ComputeIntrinsicAlignmentGrid!(CosmoGrid, WLW, Density,
     backgroundquantities, Cosmology, "../inputs/scaledmeanlum-E2Sa.txt")
     #TODO pay attention at this path!
-    ComputeWeightFunctionGrid!(WLWeightFunction, Density, CosmoGrid, backgroundquantities,
+    ComputeWeightFunctionGrid!(WLW, Density, CosmoGrid, backgroundquantities,
     Cosmology)
     CℓLL = Cℓ(CℓArray = zeros(length(CosmoGrid.ℓBinCenters),
-    length(WLWeightFunction.WeightFunctionArray[:, 1]),
-    length(WLWeightFunction.WeightFunctionArray[:, 1])))
-    ComputeCℓ!(CℓLL, WLWeightFunction, WLWeightFunction, backgroundquantities, Cosmology,
-    CosmoGrid, PowerSpectrum, CustomSimpson())
+    length(WLW.WeightFunctionArray[:, 1]),
+    length(WLW.WeightFunctionArray[:, 1])))
+    ComputeCℓ!(CℓLL, WLW, WLW, backgroundquantities, Cosmology,
+    CosmoGrid, Pmm, CustomSimpson())
     return CℓLL
 end
