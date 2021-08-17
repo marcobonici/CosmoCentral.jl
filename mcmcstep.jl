23d4dc833c6960acd80179669f6b490f92176160
Marco Bonici <marco.bonici@ge.infn.it>
2021-08-17 23:54:53 +0200

    Trying to fix distributed sampling

--- mcmcstep.jl
+++ mcmcstep.jl
@@ -13,42 +13,42 @@ for i in 1:n_process
     ClusterManagers.addprocs_lsf(1; bsub_flags = `-q long -o Turing/outfiles/$time.out -e Turing/outfiles/$time.err -n 2 `)
 end
 
+@everywhere ENV["PYTHON"] = Sys.which("python")
+using Pkg
+@everywhere using Pkg
+@everywhere Pkg.build("PyCall")
+using PyCall
+@everywhere using PyCall
 
-
-@everywhere using Turing
-@everywhere using CosmoCentral
 @everywhere w0waCDMCosmology = CosmoCentral.Flatw0waCDMCosmology()
 println("Loaded central cosmology")
 
-# Define a model on all processes.
-@everywhere @model function gdemo(CℓData, Cov, ConvolvedDensity, EuclidBias, EuclidIA,
+@everywhere @model function gdemo(vecpCℓData, Cov, ConvolvedDensity, EuclidBias, EuclidIA,
     CosmologicalGrid)
-    w₀ ~ Uniform(-0.5, -1.5)
-    wₐ ~ Uniform(0.5, 0.5)
-    Cosmology = CosmoCentral.Flatw0waCDMCosmology()
+    w₀ ~ Uniform(-1.5, -0.5)
+    wₐ ~ Uniform(-0.5, 0.5)
+    ΩM ~ Uniform(0.2, 0.4)
+    ns ~ Uniform(0.8, 1.1)
+
+    w0waCDMCosmology = CosmoCentral.Flatw0waCDMCosmology()
     w0waCDMCosmology.w0 = w₀
     w0waCDMCosmology.wa = wₐ
+    w0waCDMCosmology.ΩM = ΩM
+    w0waCDMCosmology.ns = ns
     CℓMCMC = CosmoCentral.EvaluateCℓMCMCStep(w0waCDMCosmology, ConvolvedDensity, EuclidBias,
     EuclidIA, CosmologicalGrid)
 
-    ℓnumber = length(CℓData.CℓArray[:,1,1])
-    inumber = length(CℓData.CℓArray[1,:,1])
-    L = EliminationMatrix(inumber)
+    ℓnumber = length(CℓMCMC.CℓArray[:,1,1])
+    inumber = length(CℓMCMC.CℓArray[1,:,1])
+    L = CosmoCentral.EliminationMatrix(inumber)
 
-    vecpCℓData = zeros(1,floor(Int,inumber*0.5*(inumber+1)))
     vecpCℓMCMC = zeros(1,floor(Int,inumber*0.5*(inumber+1)))
     for ℓ in 1:ℓnumber
-        vecCℓData = vec(CℓData.CℓArray[ℓ,:,:])
-        vecpCℓData = zeros(floor(Int,inumber*0.5*(inumber+1)))
-        LinearAlgebra.mul!(vecpCℓData, L, vecCℓData)
-
         vecCℓMCMC = vec(CℓMCMC.CℓArray[ℓ,:,:])
         vecpCℓMCMC = zeros(floor(Int,inumber*0.5*(inumber+1)))
         LinearAlgebra.mul!(vecpCℓMCMC, L, vecCℓMCMC)
-    end
 
-    for ℓidx in eachindex(Cl)
-        vecpCℓMCMC[ℓidx,:,:] ~ MvNormal(vecpCℓData[ℓidx,:,:], Cov.CℓCovariance[ℓidx,:,:])
+        vecpCℓData[ℓ,:] ~ MvNormal(vecpCℓMCMC, Cov.Covariance[ℓ,:,:])
     end
 end
 
@@ -79,12 +79,27 @@ KArray = CosmoCentral.LogSpaced(1e-5, 50., 1000), ℓBinCenters = MultipolesArra
 @everywhere CℓData = CosmoCentral.EvaluateCℓMCMCStep(w0waCDMCosmology, ConvolvedDensity, EuclidBias,
 EuclidIA, CosmologicalGrid)
 #TODO now only LL, but this need to be more flexible...maybe list with probes?
-@everywhere Covaₗₘ = InstantiateEvaluateCovariance(CentralCℓ, ConvolvedDensity, CosmologicalGrid, "Lensing",
+@everywhere Covaₗₘ = CosmoCentral.InstantiateEvaluateCovariance(CℓData, ConvolvedDensity, CosmologicalGrid, "Lensing",
 "Lensing")
-@everywhere CovCℓ = InstantiateEvaluateCovariance(Covaₗₘ)
+@everywhere CovCℓ = CosmoCentral.InstantiateEvaluateCovariance(Covaₗₘ)
+@everywhere inumber = 10
+@everywhere L = CosmoCentral.EliminationMatrix(inumber)
+@everywhere vecpCℓData = zeros(100,floor(Int,inumber*0.5*(inumber+1)))
+@everywhere for ℓ in 1:length(CℓData.CℓArray[:,1,1])
+    vecCℓData = vec(CℓData.CℓArray[ℓ,:,:])
+    vecpCℓDataT = zeros(floor(Int,inumber*0.5*(inumber+1)))
+    LinearAlgebra.mul!(vecpCℓDataT, L, vecCℓData)
+    vecpCℓData[ℓ,:] = vecpCℓDataT
+    Matrix = (CovCℓ.Covariance[ℓ,:,:] .+ transpose(CovCℓ.Covariance[ℓ,:,:])) ./2
+    CovCℓ.Covariance[ℓ,:,:] = Matrix
+end
+println(size(CovCℓ.Covariance[1,:,:]))
 
-@everywhere model = gdemo(CℓData, CovCℓ, ConvolvedDensity, EuclidBias, EuclidIA,
+
+
+
+@everywhere model = gdemo(vecpCℓData, CovCℓ, ConvolvedDensity, EuclidBias, EuclidIA,
 CosmologicalGrid)
 
-chains = sample(model, MH(), MCMCDistributed(), 100, n_process; save_state = true)
-write("Turing/chains/first_chain-file.jls", chains)
+chains = sample(model, MH(), MCMCDistributed(), 300, n_process; save_state = true)
+write("first_chain-file.jls", chains)
