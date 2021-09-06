5adf326d52358e6bbde6fb31a06d8e4ad8912e37
Marco Bonici <marco.bonici@ge.infn.it>
2021-09-06 18:00:45 +0200

    Fixed emulator grid call

--- src/Emulator.jl
+++ src/Emulator.jl
@@ -6,33 +6,46 @@
     DuplicationMatrix::Matrix{Float64} = zeros(10,10)
 end
 
-function SetInputNN!(array_input, inMinMax, cosmogrid::CosmoCentral.CosmologicalGrid, cosmology::CosmoCentral.w0waCDMCosmology)
-    array_input[1,:] .= (cosmology.ΩM - inMinMax[1,1])/(inMinMax[1,2]-inMinMax[1,1])
-    array_input[2,:] .= (cosmology.ΩB - inMinMax[2,1])/(inMinMax[2,2]-inMinMax[2,1])
-    array_input[3,:] .= (cosmology.H0 - inMinMax[3,1])/(inMinMax[3,2]-inMinMax[3,1])
-    array_input[4,:] .= (cosmology.ns - inMinMax[4,1])/(inMinMax[4,2]-inMinMax[4,1])
-    array_input[5,:] .= (cosmology.σ8 - inMinMax[5,1])/(inMinMax[5,2]-inMinMax[5,1])
-    array_input[6,:] .= (cosmology.w0 - inMinMax[6,1])/(inMinMax[6,2]-inMinMax[6,1])
-    array_input[7,:] .= (cosmology.wa - inMinMax[7,1])/(inMinMax[7,2]-inMinMax[7,1])
-    array_input[8,:]  = (log10.(cosmogrid.ℓBinCenters) .-inMinMax[8,1]) ./ (inMinMax[8,2]-inMinMax[8,1])
+function SetℓGrid!(cosmoemu::CℓEmulator, cosmogrid::CosmologicalGrid)
+    cosmoemu.InputParams = zeros(8, length(cosmogrid.ℓBinCenters))
+end
+
+
+function SetInputNN!(cosmoemu, cosmogrid::CosmologicalGrid, cosmology::w0waCDMCosmology)
+    cosmoemu.InputParams[1,:] .= (cosmology.ΩM - cosmoemu.InMinMax[1,1])/
+    (cosmoemu.InMinMax[1,2]-cosmoemu.InMinMax[1,1])
+    cosmoemu.InputParams[2,:] .= (cosmology.ΩB - cosmoemu.InMinMax[2,1])/
+    (cosmoemu.InMinMax[2,2]-cosmoemu.InMinMax[2,1])
+    cosmoemu.InputParams[3,:] .= (cosmology.H0 - cosmoemu.InMinMax[3,1])/
+    (cosmoemu.InMinMax[3,2]-cosmoemu.InMinMax[3,1])
+    cosmoemu.InputParams[4,:] .= (cosmology.ns - cosmoemu.InMinMax[4,1])/
+    (cosmoemu.InMinMax[4,2]-cosmoemu.InMinMax[4,1])
+    cosmoemu.InputParams[5,:] .= (cosmology.σ8 - cosmoemu.InMinMax[5,1])/
+    (cosmoemu.InMinMax[5,2]-cosmoemu.InMinMax[5,1])
+    cosmoemu.InputParams[6,:] .= (cosmology.w0 - cosmoemu.InMinMax[6,1])/
+    (cosmoemu.InMinMax[6,2]-cosmoemu.InMinMax[6,1])
+    cosmoemu.InputParams[7,:] .= (cosmology.wa - cosmoemu.InMinMax[7,1])/
+    (cosmoemu.InMinMax[7,2]-cosmoemu.InMinMax[7,1])
+    cosmoemu.InputParams[8,:]  = (log10.(cosmogrid.ℓBinCenters) .-cosmoemu.InMinMax[8,1]) ./
+    (cosmoemu.InMinMax[8,2]-cosmoemu.InMinMax[8,1])
 end
 
 function ComputeCℓ(cosmoemulator::CℓEmulator, cosmology::CosmoCentral.w0waCDMCosmology, cosmogrid::CosmoCentral.CosmologicalGrid)
-    SetInputNN!(cosmoemulator.InputParams, cosmoemulator.InMinMax, cosmogrid, cosmology)
+    SetℓGrid!(cosmoemulator, cosmogrid)
+    SetInputNN!(cosmoemulator, cosmogrid, cosmology)
     y = cosmoemulator.NN(cosmoemulator.InputParams)
-    mmm = zeros(100, 10, 10)
+    n_ℓ = length(cosmogrid.ℓBinCenters)
+    mmm = zeros(n_ℓ, 10, 10)
     c = zeros(100)
     @avx for i in 1:55
-        for l in 1:100
+        for l in 1:n_ℓ
             y[i,l] *= (cosmoemulator.OutMinMax[i,2]-cosmoemulator.OutMinMax[i,1])
             y[i,l] += (cosmoemulator.OutMinMax[i,1])
         end
     end
-    mmm = zeros(100, 10, 10)
-    c = zeros(100)
-    for i in 1:100
-        mul!(c, cosmoemulator.DuplicationMatrix, 10 .^y[:,i])
-        mmm[i,:,:] = reshape(c, (10,10))
+    for l in 1:n_ℓ
+        mul!(c, cosmoemulator.DuplicationMatrix, 10 .^y[:,l])
+        mmm[l,:,:] = reshape(c, (10,10))
     end
     return mmm
 end
