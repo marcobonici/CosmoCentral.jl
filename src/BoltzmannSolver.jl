382b3b6281c9f5fd6bce0ae5449bceb956c416cb
Marco Bonici <bonici.marco@gmail.com>
2021-12-28 18:24:22 +0100

    Still updating files

--- src/BoltzmannSolver.jl
+++ src/BoltzmannSolver.jl
@@ -1,27 +1,27 @@
 """
-    Initializeclassy(w0waCDMCosmology::w0waCDMCosmology)
+    Initializeclassy(cosmo::w0waCDMCosmology)
 
 This function, given a [`w0waCDMCosmology`](@ref), returns the
 [`classyParams`](@ref) correctly initialized.
 """
-function Initializeclassy(w0waCDMCosmology::w0waCDMCosmology)
+function Initializeclassy(cosmo::cosmo)
     classyParamsDict = Dict("output" => "mPk",
     "non linear"=> "halofit",
-    "Omega_b"=> w0waCDMCosmology.ΩB,
-    "Omega_cdm"=> w0waCDMCosmology.ΩM-w0waCDMCosmology.ΩB-
-    w0waCDMCosmology.Mν/(93.14*(w0waCDMCosmology.H0/100)^2),
+    "Omega_b"=> cosmo.ΩB,
+    "Omega_cdm"=> cosmo.ΩM-cosmo.ΩB-
+    cosmo.Mν/(93.14*(cosmo.H0/100)^2),
     "N_ur"=> 2.0328,
-    "h"=> w0waCDMCosmology.H0/100.,
-    "sigma8" => w0waCDMCosmology.σ8,
-    "n_s" => w0waCDMCosmology.ns,
-    "m_ncdm" => w0waCDMCosmology.Mν,
+    "h"=> cosmo.H0/100.,
+    "sigma8" => cosmo.σ8,
+    "n_s" => cosmo.ns,
+    "m_ncdm" => cosmo.Mν,
     "P_k_max_1/Mpc" => 50.,
     "z_max_pk" =>  5.,
     "use_ppf" =>  "yes",
-    "w0_fld" =>  w0waCDMCosmology.w0,
-    "Omega_k" =>  w0waCDMCosmology.Ωk,
-    "Omega_fld" =>  w0waCDMCosmology.ΩDE,
-    "wa_fld" =>  w0waCDMCosmology.wa,
+    "w0_fld" =>  cosmo.w0,
+    "Omega_k" =>  cosmo.Ωk,
+    "Omega_fld" =>  cosmo.ΩDE,
+    "wa_fld" =>  cosmo.wa,
     "cs2_fld" =>  1.,
     "N_ncdm" =>  1,
     "tau_reio" =>  0.058)
@@ -57,22 +57,22 @@ end
 
 
 """
-    EvaluatePowerSpectrum!(classyParams:: classyParams, CosmologicalGrid::CosmologicalGrid,
-    PowerSpectrum::PowerSpectrum)
+    EvaluatePowerSpectrum!(classyParams:: classyParams, cosmogrid::CosmologicalGrid,
+    pmm::PowerSpectrum)
 
 This function runs classy to evaluate the Matter Power Spectrum over the ``k-z``
 grid specified in [`CosmologicalGrid`](@ref)
 """
 function EvaluatePowerSpectrum!(classyParams:: classyParams,
-    CosmologicalGrid::CosmologicalGrid, PowerSpectrum::PowerSpectrum)
+    cosmogrid::CosmologicalGrid, pmm::PowerSpectrum)
     cosmo = classy.Class()
     cosmo.set(classyParams.classyParamsDict)
     cosmo.compute()
-    for (idxz, myz) in enumerate(CosmologicalGrid.ZArray)
-        for (idxk, myk) in enumerate(CosmologicalGrid.KArray)
-            PowerSpectrum.PowerSpectrumLinArray[idxk, idxz] =
+    for (idxz, myz) in enumerate(cosmogrid.ZArray)
+        for (idxk, myk) in enumerate(cosmogrid.KArray)
+            pmm.PowerSpectrumLinArray[idxk, idxz] =
             cosmo.pk_lin(myk, myz)
-            PowerSpectrum.PowerSpectrumNonlinArray[idxk, idxz] =
+            pmm.PowerSpectrumNonlinArray[idxk, idxz] =
             cosmo.pk(myk, myz)
         end
     end
